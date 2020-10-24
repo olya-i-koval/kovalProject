ab8a1a99ac8f175104229a2a2344c8d2a1b9ae70
koval-olya3 <63600652+koval-olya3@users.noreply.github.com>
2020-10-24 19:23:25 +0300

    Add task1401 for lab14

---
+++ src/main/java/ua/edu/npu/lab14/task1401/App.java
@@ -0,0 +1,14 @@
+package ua.edu.npu.lab14.task1401;
+
+import java.util.concurrent.Semaphore;
+
+public class App {
+    public static void main(String[] args) {
+        Semaphore book = new Semaphore(1);
+
+        // new Thread(String.valueOf(new IncThread("A", book))).start();
+        new Thread(new MyThread("men", book)).start();
+        new Thread(new MyThread("men 2 ", book)).start();
+
+    }
+}
