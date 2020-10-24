c073cca96b339bfe6e6844980d5173cdbf19b657
koval-olya3 <63600652+koval-olya3@users.noreply.github.com>
2020-10-24 21:13:50 +0300

    Add task1402 for lab14

---
+++ src/main/java/ua/edu/npu/lab14/task1402/Consumer.java
@@ -0,0 +1,19 @@
+package ua.edu.npu.lab14.task1402;
+
+import ua.edu.npu.lab14.task1402.Q;
+
+public class Consumer implements Runnable {
+
+    Q q;
+
+
+    Consumer (Q q){
+        this.q =q;
+    }
+
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            q.get(2);
+        }
+    }
+}
