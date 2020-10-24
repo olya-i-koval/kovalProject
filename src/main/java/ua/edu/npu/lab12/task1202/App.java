c317522b1ae599e8d524ac264920f2154e56ed6b
koval-olya3 <63600652+koval-olya3@users.noreply.github.com>
2020-10-24 19:08:49 +0300

    Add

---
+++ src/main/java/ua/edu/npu/lab12/task1202/App.java
@@ -0,0 +1,16 @@
+package ua.edu.npu.lab12.task1202;
+
+public class App {
+    public static void main(String[] args) {
+        try {
+            System.out.println(5/0);
+        }
+        catch (Exception e) {
+            e.toString();
+        }
+
+    }
+
+}
+
+
